bc0032ced415642f0a5a70eabaf8eabe39c43f97
bovao <alexis.grimonprez@gmail.com>
2017-06-09 00:23:24 +0200

    modifie champs detailTache

--- secretaire/detailTache.php
+++ secretaire/detailTache.php
@@ -22,10 +22,11 @@ extract($tache);
         <input type="button" name="societe" value="Societe : <?php if(isset($societe)) echo $societe; ?>" class="custom-input3">
         <input type="button" name="client" value="Client : <?php if(isset($client)) echo $client; ?>" class="custom-input3">
         <input type="button" name="adresse" value="<?php if(isset($adresse)) echo $adresse; ?>" class="custom-input3">
-        <input type="button" name="secteur" value="Secteur : <?php if(isset($secteur)) echo $secteur; ?>" class="custom-input3">
+         <input type="button" name="libelle" value="Libelle : <?php if(isset($libelle)) echo $libelle; ?>" class="custom-input3">
         <input type="button" name="duree" value="Durée : <?php if(isset($duree)) echo $duree; ?>" class="custom-input3">
-
+        <input type="button" name="secteur" value="Secteur : <?php if(isset($secteur)) echo $secteur; ?>" class="custom-input3">
         <input type="button" name="assignea" value="Assigné à : <?php if(isset($assignea)) echo $assignea; ?>" class="custom-input3">
+        <input type="button" name="etat" value="Etat : <?php if(isset($etat)) echo $etat; ?>" class="custom-input3">
     </div><div class="retourligne"></div>
     
     <textarea placeholder="Votre message..." class="custom-textarea2" disabled>
