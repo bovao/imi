1fc0c989ed1734a4bdaf35537c4159d08933e44c
bovao <alexis.grimonprez@gmail.com>
2017-06-12 14:56:17 +0200

    ajoute intervention en fonction d'un tâche

--- technicien/intervenir.php
+++ technicien/intervenir.php
@@ -3,13 +3,43 @@
 require_once("../includes/fonctions.php");
 $db = connect(); 
 session_start();
-?>
+
+
+//Insertion des données
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   // copieNB, copieCouleur, arrive, depart, s, statut
+
+  $copieNB = verifChampRempli("copieNB");
+  $copieCouleur = verifChampRempli("copieCouleur");
+  $arrive = verifChampRempli("arrive");
+  $depart = verifChampRempli("depart");
+  $descriptionIntervention = verifChampRempli("descriptionIntervention");
+  $statut = verifChampRempli("statut");
     
+if(empty($erreur)) {
+      $sql = "INSERT INTO interventions VALUES (NULL, :copieNB, :copieCouleur, :arrive, :depart, :descriptionIntervention, :statut, NOW())";
+  try {
+    $req = $db->prepare($sql);
+        $req->bindParam(':copieNB', $copieNB, PDO::PARAM_INT);
+        $req->bindParam(':copieCouleur', $copieCouleur, PDO::PARAM_INT);
+        $req->bindParam(':arrive', $arrive, PDO::PARAM_INT);
+        $req->bindParam(':depart', $depart, PDO::PARAM_INT);
+        $req->bindParam(':descriptionIntervention', $descriptionIntervention, PDO::PARAM_STR);    
+        $req->bindParam(':statut', $statut, PDO::PARAM_STR); 
+        $req->execute();
+        echo "<script>alert(\"Nouvelle interventions ajouté !\")</script>"; 
+        header("location:intervenir.php?add=ok");
+  } catch (PDOException $erreur) {
+        echo $erreur->getMessage();
+    }
+  
+}  //end empty(erreur)   
 
 
-<body>
+} //end $_SERVER post 
 
-<?php
+
+
 if(!empty($erreur)) {
     echo "<p class='erreur'>";
     foreach ($erreurs as $value) {
@@ -20,6 +50,8 @@ if(!empty($erreur)) {
 ?>
 
 
+<body>
+
 <div id="bloc_page">
     
 <div class="row entete"> 
@@ -28,12 +60,13 @@ if(!empty($erreur)) {
 </div>
     
 <form action="" method="POST">
+  <input type="hidden" name="id" value="<?php echo intval($_GET["id"]); ?>">
     <div class="row">
         <div class="col3">
          <fieldset><legend>Détails :</legend> 
             <input type="hidden" name="id" value="<?php echo intval($_GET["id"]); ?>">
              
-             <input <?php if(isset($erreurs['copieNB'])) echo "class='erreur'"; ?>type="text" name="copieNB" value="<?php if(isset($copieNB)) echo $copieNB; ?>" placeholder="Nb copie N&B" class="custom-input" /> <!-- pseudo -->
+             <input <?php if(isset($erreurs['copieNB'])) echo "class='erreur custom-input'"; ?>type="text" name="copieNB" value="<?php if(isset($copieNB)) echo $copieNB; ?>" placeholder="Nb copie N&B" class="custom-input" /> <!-- pseudo -->
              
 
             <input type="text" name="copieCouleur" placeholder="Nb copie couleur" class="custom-input" 
@@ -65,54 +98,50 @@ if(!empty($erreur)) {
 <script src="https://use.fontawesome.com/39cfcc7a15.js"></script>
 </body>
 
-<?php 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-   // copieNB, copieCouleur, arrive, depart, s, statut
-
-  $copieNB = verifChampRempli("copieNB");
-  $copieCouleur = verifChampRempli("copieCouleur");
-  $arrive = verifChampRempli("arrive");
-  $depart = verifChampRempli("depart");
-  $descriptionIntervention = verifChampRempli("descriptionIntervention");
-  $statut = verifChampRempli("statut");
-    
-if(empty($erreur)) {
-      $sql = "INSERT INTO interventions VALUES (NULL, :copieNB, :copieCouleur, :arrive, :depart, :descriptionIntervention, :statut, NOW())";
-
+<?php
+function getInterventionsDetails($db, $id) {
+  $sql = "SELECT * FROM interventions WHERE id = :id";
   try {
+    // On prépare la requête : elle est envoyée au serveur sans les données variables
     $req = $db->prepare($sql);
-        $req->bindParam(':copieNB', $copieNB, PDO::PARAM_INT);
-        $req->bindParam(':copieCouleur', $copieCouleur, PDO::PARAM_INT);
-        $req->bindParam(':arrive', $arrive, PDO::PARAM_INT);
-        $req->bindParam(':depart', $depart, PDO::PARAM_INT);
-        $req->bindParam(':descriptionIntervention', $descriptionIntervention, PDO::PARAM_STR);    
-        $req->bindParam(':statut', $statut, PDO::PARAM_STR); 
-      
-        $req->execute();
-        header("location:intervenir.php?intervention=ok");
-        echo "<script>alert(\"Nouvelle intervention effectuée !\")</script>"; 
+    // On lie la donnée récupérée en GET avec notre requête préparée, et on déclare qu'elle doit être un entier.
+    $req->bindParam(':id', $id, PDO::PARAM_INT);
+    // Exécution de la requête
+    $req->execute();
+    // Je récupère l'ensemble des données retournées par la requête grâce à fetchAll
+    $interventions = $req->fetchAll()[0];
+    // j'assigne ces données à mes variables utilisées dans mon formulaire
+    return $interventions;
+
   } catch (PDOException $erreur) {
+    echo $erreur->getMessage();
+  }
+}
+
+$interventions = getInterventionsDetails($db, $_GET['id']);
+extract($interventions);
+
+//Recup le detail de la tâche pour mettre à jour le statut de la tâche afin de la basculé et ne plus l'affiche ds Taches.php
+   $tache = getDetailsTache($db, $_GET["id"]);
+   extract($tache);
+
+//met à jour l'état de la tache 
+if($statut == "tachesEffectuee"){
+     $sql = "UPDATE taches SET id = :id, etat = 'tachesEffectuee' WHERE id = :id";
+     try {
+        $req = $db->prepare($sql);
+        $req->bindParam(':id', $id, PDO::PARAM_INT);
+        $req->bindParam(':statut', $statut, PDO::PARAM_STR);
+        $req->execute();
+        echo "<script>alert(\"Taches mise à jour !\")</script>"; 
+        header("location:taches.php?edit=ok");
+     }//end try
+    catch (PDOException $erreur) {
         echo $erreur->getMessage();
     }
-  
-    if($_POST["statut"] == "tachesEffectuee"){
-         $sql = "UPDATE taches SET etat = :etat WHERE etat = 'TacheEffectue' and id = :id  ";
-         try {
-            $req = $db->prepare($sql);
-            $req->bindParam(':statut', $statut, PDO::PARAM_STR);
-            $req->execute();
-            header("location:taches.php?intervention=ok");
-            echo "<script>alert(\"Taches mise à jour !\")</script>"; 
-         }//end try
-        catch (PDOException $erreur) {
-            echo $erreur->getMessage();
-        }
-    }//end if
+}//end if
 
-}  //end empty(erreur)   
-
-
-}
 ?>
+
 
     
